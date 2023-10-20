34117ca5d8e2f2f953680f6f042d59eaeb18d903
Marney Pratt <mcpratt@smith.edu>
2023-10-20 13:08:19 -0400

    updated to base pipe

--- script_templates/data_wrangling/density.all.R
+++ script_templates/data_wrangling/density.all.R
@@ -3,15 +3,15 @@
 
 # macros and master.taxa data files must be imported before you can run the code below
 
-macro.density <- macros %>% 
+macro.density <- macros  |>  
   
   #join taxa info
-  left_join(., master.taxa) %>%
+  left_join(master.taxa)  |> 
   
   # Sum for each sampleID and the different taxa 
   # density of ALL macroinvertebrates
   # change group_by function to remove or add grouping variables as needed 
-  group_by(date, sampleID, season, location, year, benthicArea) %>% 
+  group_by(date, sampleID, season, location, year, benthicArea)  |>  
   dplyr::summarise (density = sum(invDens, na.rm = TRUE)) 
 
 
--- script_templates/data_wrangling/density.env.R
+++ script_templates/data_wrangling/density.env.R
@@ -1,27 +1,26 @@
-
 # make sure the 'tidyverse' package is installed and loaded to run the code below
 
 # macros, master.taxa, and env must all be imported before you can run the code below
 
-macro.density <- macros %>% 
+macro.density <- macros |> 
   
   #join taxa info
-  left_join(., master.taxa) %>%
-  
+  left_join(master.taxa) |>
   
   #join the env data
-  left_join(., env) %>% 
+  left_join(env) |> 
   
   # Sum for each sampleID and the different taxa 
   # density of ALL macroinvertebrates
-  group_by(date, sampleID, season, year, location, benthicArea) %>% 
-  dplyr::summarise (density = sum(invDens, na.rm = TRUE),
+  group_by(date, sampleID, season, year, location, benthicArea) |> 
+  dplyr::summarise(density = sum(invDens, na.rm = TRUE),
+            mon.ADD = round(mean(mon.ADD, na.rm = TRUE), 
+                                  digits = 0),
+            mon.precip = round(mean(mon.precip, na.rm = TRUE),
+                                       digits = 0),
                     
-                    mon.ADD = round(mean(mon.ADD, na.rm=TRUE), digits=0),
-                    mon.precip = round(mean(mon.precip, na.rm=TRUE), digits=0),
-                    
-                    # replace the blanks below with your water quality variable of interest
-                    ___ = mean(___, na.rm = TRUE)) %>% 
+#replace the blanks below with your water quality variable of interest
+                    ___ = mean(___, na.rm = TRUE)) |> 
   
   #remove NA's for your water quality variable of interest
   dplyr::filter(!is.na(___))
