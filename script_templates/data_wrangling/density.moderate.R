bf02793ca72e34690cb61183b0bfd77d054ce4b7
Marney Pratt <mcpratt@smith.edu>
2023-10-27 17:05:52 -0400

    updated to base pipe and edited for easier use

--- script_templates/data_wrangling/density.moderate.R
+++ script_templates/data_wrangling/density.moderate.R
@@ -3,51 +3,52 @@
 # macros, master.taxa, and env data files must be imported before you 
 # can run the code below
 
-#get sample info
-sample.info <- macros %>%
-  select(date, sampleID, season, year, location, benthicArea) %>% 
+#get sample info and env variables of interest
+sample.info <- macros |>
+  
+  #join environmental variables
+  left_join(env) |> 
+  
+  #select variables of interest
+  #delete anything you don't need
+  #add anything you do need in the blank with commas in between
+  dplyr::select(date, sampleID, season, year, location, benthicArea,
+                ___) |> 
   distinct()
 
 #get total samples
-samples <- macros %>%
-  select(sampleID) %>% 
+samples <- macros |>
+  select(sampleID) |> 
   distinct()
 
-
-#select env variable(s) you want by replacing the blank with the column name(s)
-env.vars <- env %>% 
-  select(sampleID, ___)
-
-
 #calculate density for each sampleID
-moderate.density <- macros %>% 
+moderate.density <- macros |> 
   
-  #join taxa info
-  left_join(., master.taxa) %>%
+  #join taxa & tolerance info
+  left_join(master.taxa) |>
   
   #filter for moderately tolerant macros (4-7) 
-  dplyr::filter(tolerance > 3, tolerance < 8) %>% 
+  dplyr::filter(tolerance > 3, tolerance < 8) |> 
   
-  # Sum for each sampleID  
-  # density of filtered macroinvertebrates
-  # change group_by function to remove or add grouping variables as needed 
-  group_by(sampleID) %>% 
-  dplyr::summarise (density = sum(invDens, na.rm = TRUE)) %>% 
+  #filter out the midges
+  dplyr::filter(family != "Chironomidae") |> 
+  
+  # Summarize for each sampleID 
+  # density of ALL macroinvertebrates
+  group_by(sampleID)  |>  
+  dplyr::summarise(density = sum(invDens, na.rm = TRUE)) |> 
   
   #add back in samples that had none present
-  right_join(., samples) %>% 
+  right_join(samples) |> 
   
   #fill in group combos where there were none present with zeros
-  ungroup() %>% 
+  ungroup() |> 
   complete(sampleID,  
-           fill = list(density = 0)) %>% 
+           fill = list(density = 0)) |> 
   
   #add back the sampling info
-  left_join(., sample.info) %>% 
+  left_join(sample.info) |> 
   
-  #add env variables
-  left_join(., env.vars)
-
-
-
-
+  #filter out anything you don't want
+  #the example below would filter out just the year 2018
+  dplyr::filter(year != "2018") 
