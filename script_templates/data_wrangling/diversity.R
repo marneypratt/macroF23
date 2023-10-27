1d94ea1807018688d635663e8b8c68d34ef39475
Marney Pratt <mcpratt@smith.edu>
2023-10-27 17:30:00 -0400

    updated to base pipe and edited for ease of use

--- script_templates/data_wrangling/diversity.R
+++ script_templates/data_wrangling/diversity.R
@@ -4,57 +4,64 @@
 # macros and master.taxa data files must both be imported before you can run the code below
 
 # remove missing values, and make sure each taxon is summarized within sampleID
-macro.long <- macros %>%  
+macro.long <- macros |>  
   
   #join taxa info
-  left_join(., master.taxa) %>%
+  left_join(master.taxa) |>
   
-  dplyr::filter(!is.na(invDens), year !="2018") %>% 
-  dplyr::select(sampleID, organism_aggr, invDens) %>% 
-  group_by(sampleID, organism_aggr) %>% 
+  #diversity for mix of taxa (organism_aggr)
+  #change organism_aggr to family or other taxonomic unit if desired
+  dplyr::filter(!is.na(invDens), year !="2018") |> 
+  dplyr::select(sampleID, organism_aggr, invDens) |> 
+  group_by(sampleID, organism_aggr) |> 
   dplyr::summarise(density = sum(invDens))
 
 
 # convert to wide format
-macro.wide <- macro.long %>% 
+macro.wide <- macro.long |> 
   pivot_wider(names_from = organism_aggr, 
               values_from = density,
               values_fill = list(density = 0),
-              values_fn = list(density = sum)) %>%
+              values_fn = list(density = sum)) |>
   tibble::column_to_rownames("sampleID")
 
 
 #Calculate diversity index values
 
-#Shannon
-H <- diversity(macro.wide)
+#Shannon index (H)
+shannon <- diversity(macro.wide)
 
 
 #effective number of species
-effective.sp <- exp(H)
+effective.sp <- exp(shannon)
 
 
 #Richness
-rich <- specnumber(macro.wide) 
+richness <- specnumber(macro.wide) 
 
 #Max possible diversity
 max.H <- log(rich)
 
 
 #Pielou's Evenness J 
-J <- H / max.H
+evenness <- shannon / max.H
 
 #put all diversity values into a single data frame
-macro.div <- data.frame(H, effective.sp, rich, max.H, J) %>% 
+macro.div <- data.frame(shannon, effective.sp, richness, max.H, evenness) |> 
   tibble::rownames_to_column("sampleID")
 
 
 # select other variables you want present in your final dataset
-variables <- macros %>% 
+variables <- macros |> 
   
-  #add or remove any variables from the original dataset that you want present
-  #make sure you keep sampleID because this is what is used to match the data
-  select(sampleID, date, location, year, season, benthicArea) %>% 
+  #join environmental variables
+  left_join(env) |> 
+  
+  #select variables of interest
+  #delete anything you don't need
+  #add anything you do need in the blank with commas in between
+  dplyr::select(date, sampleID, season, year, location, benthicArea,
+                DO) |> 
   distinct()
 
 #this script takes the macro.div and joins it with the mydf one we just created
